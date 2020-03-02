05cd7b3e11a4d3787e8ff96c7dd0ea61ae90a80e
FABRICIO <fnmamao@gmail.com>
2020-03-02 19:49:34 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mongagua");
+            Console.WriteLine("itanhaem");
+            Console.WriteLine("peruibe");
+
+        }
+    }
+}
